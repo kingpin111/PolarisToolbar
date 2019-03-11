26b712a2810de47522a40c26b52af0c2cb106514
Victor K. Lee <victor@Victors-Mac.local>
2019-03-12 01:05:01 +0900

    Default UILayout Commit

--- PolarisToolbar/ViewController.swift
+++ PolarisToolbar/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainToolbar: UIView!
+    @IBOutlet var drawToolbar: UIView!
+    @IBOutlet var findToolbar: UIView!
+    @IBOutlet var drawToolbarToggleBtn: UIButton!
+    @IBOutlet var findToolbarToggleBtn: UIButton!
+    var drawToolbarHeightConstraint: NSLayoutConstraint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func toggleDrawToolbar(_ sender: Any) {
+        drawToolbarHeightConstraint?.isActive = false
+        
+        if drawToolbar.frame.height != 0 {
+            drawToolbarHeightConstraint = drawToolbar.heightAnchor.constraint(equalToConstant: 0)
+            drawToolbarToggleBtn.setTitle("Show DrawToolbar", for: .normal)
+        } else {
+            drawToolbarHeightConstraint = drawToolbar.heightAnchor.constraint(equalToConstant: 44)
+            drawToolbarToggleBtn.setTitle("Hide DrawToolbar", for: .normal)
+        }
+        UIView.animate(withDuration: 1) {
+            self.drawToolbarHeightConstraint?.isActive = true
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction func toggleFindToolbar(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onTapped(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
 }
 

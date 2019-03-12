41ef6b04b07b10d71c23d8e238a282d4669558ab
Victor <kingpin11@naver.com>
2019-03-12 14:01:26 +0900

    Thumbnail View Size Class implement in IB

--- PolarisToolbar/ViewController.swift
+++ PolarisToolbar/ViewController.swift
@@ -15,7 +15,16 @@ class ViewController: UIViewController {
     @IBOutlet var findToolbar: UIView!
     @IBOutlet var drawToolbarToggleBtn: UIButton!
     @IBOutlet var findToolbarToggleBtn: UIButton!
+    @IBOutlet weak var slideNote: UIView!
     var drawToolbarHeightConstraint: NSLayoutConstraint?
+    @IBOutlet weak var slideNoteHeightConstraint: NSLayoutConstraint!
+    
+    override public var traitCollection: UITraitCollection {
+        if UIDevice.current.userInterfaceIdiom == .pad && UIDevice.current.orientation.isPortrait {
+            return UITraitCollection(traitsFrom:[UITraitCollection(horizontalSizeClass: .compact), UITraitCollection(verticalSizeClass: .regular)])
+        }
+        return super.traitCollection
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +41,7 @@ class ViewController: UIViewController {
             drawToolbarHeightConstraint = drawToolbar.heightAnchor.constraint(equalToConstant: 44)
             drawToolbarToggleBtn.setTitle("Hide DrawToolbar", for: .normal)
         }
-        UIView.animate(withDuration: 1) {
+        UIView.animate(withDuration: 0.3) {
             self.drawToolbarHeightConstraint?.isActive = true
             self.view.layoutIfNeeded()
         }
@@ -41,10 +50,27 @@ class ViewController: UIViewController {
     @IBAction func toggleFindToolbar(_ sender: Any) {
         
     }
+    @IBAction func toggleSlideNote(_ sender: Any) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.slideNoteHeightConstraint.constant = 0
+            self.view.layoutIfNeeded()
+        }) { (isFinished) in
+            self.slideNote.isHidden = true
+        }
+    }
     
     @IBAction func onTapped(_ sender: Any) {
         view.endEditing(true)
     }
     
+    override func overrideTraitCollection(forChild childViewController: UIViewController) -> UITraitCollection? {
+        if view.bounds.width  < view.bounds.height {
+            let traitCollections = [UITraitCollection(horizontalSizeClass: .compact), UITraitCollection(verticalSizeClass: .regular)]
+            return  UITraitCollection(traitsFrom: traitCollections)
+        } else {
+            let traitCollections = [UITraitCollection(horizontalSizeClass: .unspecified), UITraitCollection(verticalSizeClass: .unspecified)]
+            return  UITraitCollection(traitsFrom: traitCollections)
+        }
+    }
 }
 
